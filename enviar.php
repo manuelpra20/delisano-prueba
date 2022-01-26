2c8026670b515a6007c1f23588405c1cb304e55b
manuelpra20 <manuelpra@gmail.com>
2022-01-26 06:04:30 -0400

    agregue informacion sobre nosotros

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php  
+
+// Llamando a los campos
+// $nombre = $_POST['nombre'];
+// $correo = $_POST['correo'];
+// $telefono = $_POST['telefono'];
+// $mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+// $destinatario = "manuelpra20@gmail.com";
+// $asunto = "Contact from Delisanoca";
+
+// $carta = "from: $nombre \n";
+// $carta .= "Email: $correo \n";
+// $carta .= "Telefono: $telefono \n";
+// $carta .= "Message: $mensaje";
+
+// Enviando Mensaje
+// mail($destinatario, $asunto, $carta);
+// header('Location:email-sent.html');
+
+?>
